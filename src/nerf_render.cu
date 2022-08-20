2e28a33cc6dfa4d74edd912a2a10bf4b1274b337
Weixuan Guo <cnmozzie@gmail.com>
2022-08-20 13:48:37 +0800

    feat: add some function

--- src/nerf_render.cu
+++ src/nerf_render.cu
@@ -22,6 +22,8 @@
 #include <typeinfo>
 #include <vector>
 
+#define maxThreadsPerBlock 1024
+
 using namespace Eigen;
 using namespace tcnn;
 namespace fs = filesystem;
@@ -70,16 +72,54 @@ Eigen::Matrix<float, 4, 4> pose_spherical(float theta, float phi, float radius) 
   return c2w;
 }
 
+// line 229-249 @ efficient-nerf-render-demo/example-app/example-app.cpp
+__global__ void set_z_vals(float* z_vals, const int N ) {
+  const int index = threadIdx.x + blockIdx.x * blockDim.x;
+  if (index >= N) {
+    return;
+  }
+  float near = 2.0; // val_dataset.near
+  float far = 6.0; // val_dataset.far
+  z_vals[index] = near + (far-near) / (N-1) * index;
+}
+
+// line 251-254 @ efficient-nerf-render-demo/example-app/example-app.cpp
+__global__ void set_xyz_coarse(MatrixView<float> xyz_coarse, 
+                               MatrixView<float> rays_o,
+                               MatrixView<float> rays_d,
+                               float* z_vals, 
+                               int N_rays, 
+                               int N_samples_coarse) {
+  const int i = threadIdx.x + blockIdx.x * blockDim.x;
+  const int j = threadIdx.y + blockIdx.y * blockDim.y;
+  if (i >= N_rays || j >= N_samples_coarse) {
+    return;
+  }
+  xyz_coarse(i*N_samples_coarse+j, 0) = rays_o(i, 0) + z_vals[j] * rays_d(i, 0);
+  xyz_coarse(i*N_samples_coarse+j, 1) = rays_o(i, 1) + z_vals[j] * rays_d(i, 1);
+  xyz_coarse(i*N_samples_coarse+j, 2) = rays_o(i, 2) + z_vals[j] * rays_d(i, 2);
+}
+
 void NerfRender::render_rays(int N_rays,
                              tcnn::GPUMatrixDynamic<float>& rays_o,
                              tcnn::GPUMatrixDynamic<float>& rays_d,
                              int N_samples=128, 
                              int N_importance=5, 
                              float perturb=0.) {
-  // TODO
-  // line 287-292 @ efficient-nerf-render-demo/example-app/example-app.cpp
-  // use cuda to speed up
   std::cout << "render_rays" << std::endl;
+  int N_samples_coarse = N_samples;
+  tcnn::GPUMemory<float> z_vals_coarse(N_samples_coarse);
+  set_z_vals<<<div_round_up(N_samples_coarse, maxThreadsPerBlock), maxThreadsPerBlock>>> (z_vals_coarse.data(), N_samples_coarse);
+
+  tcnn::GPUMatrixDynamic<float> xyz_coarse(N_rays*N_samples_coarse, 3, tcnn::RM);
+  dim3 threadsPerBlock(maxThreadsPerBlock/32, 32);
+  dim3 numBlocks(div_round_up(N_rays, int(threadsPerBlock.x)), div_round_up(N_samples_coarse, int(threadsPerBlock.y)));
+  set_xyz_coarse<<<numBlocks, threadsPerBlock>>>(xyz_coarse.view(), rays_o.view(), rays_d.view(), z_vals_coarse.data(), N_rays, N_samples_coarse);
+
+  
+  float host_data[N_samples_coarse];
+  z_vals_coarse.copy_to_host(host_data);
+  std::cout << host_data[0] << " " << host_data[1] << " " << host_data[2] << std::endl;
 }
 
 void NerfRender::generate_rays(int w,
